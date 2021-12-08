68e0f169473b49e92813c869ed888cd7a2b91954
benman604 <benjamin.man1001@gmail.com>
2021-12-07 23:03:47 -0800

    ch 20

---
+++ LabSessionExtraCredit/Chapter20/CodingQuestions20/src/MultiColoredRobot.java
@@ -0,0 +1,57 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Rectangle;
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class MultiColoredRobot extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MultiColoredRobot frame = new MultiColoredRobot();
+					frame.setVisible(true);
+					frame.repaint();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MultiColoredRobot() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 400, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+	
+    @Override
+    public void paint(Graphics g) 
+    {
+    	g.setColor(Color.GREEN);
+        g.fillOval(200-105/2, 100, 105, 75);
+        g.setColor(Color.RED);
+        g.fillRect(200-50/2, 170, 50, 100);
+        g.fillOval((200-105/2) + 70, 120, 20, 20);
+        g.fillOval((200-105/2) + 20, 120, 20, 20);
+        g.setColor(Color.BLACK);
+        g.fillRect((200-105/2) + 45, 150, 50, 10);
+    }
+}
---
+++ LabSessionExtraCredit/Chapter20/CodingQuestions20/src/NoSmokingSign.java
@@ -0,0 +1,66 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.border.BevelBorder;
+
+public class NoSmokingSign extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NoSmokingSign frame = new NoSmokingSign();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public NoSmokingSign() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		contentPane.add(panel, gbc_panel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.RED);
+		panel.add(panel_1);
+		
+		JLabel lblNewLabel = new JLabel("NO SMOKING");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 44));
+		panel_1.add(lblNewLabel);
+	}
+
+}
